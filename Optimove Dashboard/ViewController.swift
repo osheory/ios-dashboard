4d0a627758b033eb0727e1af962eaadc099e2634
ohad <311492+osheory@users.noreply.github.com>
2025-09-09 12:30:02 +0300

    Initial Commit

---
+++ Optimove Dashboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Optimove Dashboard
+//
+//  Created by Ohad Sheory on 09/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Optimove DashboardTests/Optimove_DashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Optimove_DashboardTests.swift
+//  Optimove DashboardTests
+//
+//  Created by Ohad Sheory on 09/09/2025.
+//
+
+import Testing
+@testable import Optimove_Dashboard
+
+struct Optimove_DashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
